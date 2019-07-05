2cca1e6b98627759ac02573675e6e3b78d748f8d
Josmarsa <52434741+Josmarsa@users.noreply.github.com>
2019-07-04 21:22:35 -0500

    Add files via upload

---
+++ Patronesdediseño/Facade/Bank.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    class Bank
+    {
+        public bool HasSufficientSavings(Customer c, int amount)
+        {
+            Console.WriteLine("Check bank for " + c.Name);
+            return true;
+        }
+    }
+
+    /// <summary>
+
+    /// The 'Subsystem ClassB' class
+
+    /// </summary>
+
+    class Credit
+    {
+        public bool HasGoodCredit(Customer c)
+        {
+            Console.WriteLine("Check credit for " + c.Name);
+            return true;
+        }
+    }
+    
+}
